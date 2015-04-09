ccde8362c6f9e11f769d7096170b60278006c533
Enrique Zamudio <alterbass@gmail.com>
2015-04-09 17:46:01 -0500

    run ceylon.json benchmarks

--- ceylon-bench.sh
+++ ceylon-bench.sh
@@ -1,8 +1,13 @@
 echo Compiling JS
-../ceylon-dist/dist/bin/ceylon compile-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/*
+../ceylon-dist/dist/bin/ceylon compile-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/* resource/json/bench/*
 echo Compiling JVM
-../ceylon-dist/dist/bin/ceylon compile --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/*
+../ceylon-dist/dist/bin/ceylon compile --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/* resource/json/bench/*
 echo RUNNING ON JAVASCRIPT
 ../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
 echo RUNNING ON JVM
 ../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
+
+echo JSON benchmark JS
+../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run json json.bench/0.1
+echo RUNNING ON JVM
+../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run json.bench::json json.bench/0.1
