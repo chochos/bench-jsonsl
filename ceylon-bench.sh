56383e069644b08452ae8126c5c55f1b7ef5a13c
Enrique Zamudio <alterbass@gmail.com>
2015-04-09 14:28:24 -0500

    improve messages

--- ceylon-bench.sh
+++ ceylon-bench.sh
@@ -1,10 +1,8 @@
+echo Compiling JS
 ../ceylon-dist/dist/bin/ceylon compile-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/*
+echo Compiling JVM
 ../ceylon-dist/dist/bin/ceylon compile --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/*
-echo #########################
-echo # RUNNING ON JAVASCRIPT #
-echo #########################
+echo RUNNING ON JAVASCRIPT
 ../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
-echo #########################
-echo # RUNNING ON JVM        #
-echo #########################
+echo RUNNING ON JVM
 ../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
