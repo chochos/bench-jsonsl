a01f735cc73b5b7b895e9d0635a489fa13881750
Enrique Zamudio <alterbass@gmail.com>
2015-04-10 10:17:53 -0500

    simplify the runner

--- ceylon-bench.sh
+++ ceylon-bench.sh
@@ -1,13 +1,14 @@
+if [ "$1" == "" ]; then
+  WHAT=run
+else
+  WHAT=$1
+fi
+
 echo Compiling JS
 ../ceylon-dist/dist/bin/ceylon compile-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/* resource/json/bench/*
 echo Compiling JVM
 ../ceylon-dist/dist/bin/ceylon compile --rep=../ceylon-sdk/modules --rep=../jsonsl/modules source/json/bench/* resource/json/bench/*
-echo RUNNING ON JAVASCRIPT
-../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
-echo RUNNING ON JVM
-../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules json.bench/0.1
-
-echo JSON benchmark JS
-../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run json json.bench/0.1
-echo RUNNING ON JVM
-../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run json.bench::json json.bench/0.1
+echo JAVASCRIPT $WHAT
+../ceylon-dist/dist/bin/ceylon run-js --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run $WHAT json.bench/0.1
+echo JVM $WHAT
+../ceylon-dist/dist/bin/ceylon run --rep=../ceylon-sdk/modules --rep=../jsonsl/modules --run json.bench::$WHAT json.bench/0.1
